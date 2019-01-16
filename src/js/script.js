e7af35884bb1c1060604fb6f8b033820d128bf40
Carlo <carloanselmi81@gmail.com>
2019-01-16 17:39:51 +0100

    Fixed some bugs.

--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,9 @@ let questionAnswers = document.querySelector('.question__answers');
 let questionCount = document.querySelector('.status__count');
 let scoreText = document.querySelector('.status__score');
 let nextQuestionBtn = document.querySelector('.btn-next');
+let endGameH2 = document.querySelector('.end-game');
+let btnNew = document.querySelector('.btn-new');
+let score = 0;
 
 
 
@@ -35,6 +38,7 @@ let getQuestion = async () => {
 
     let waitingData = async () => {
         await getQuestion();
+        questionNumber = 0;
         renderQuestion(questionNumber);
     }
 
@@ -46,14 +50,18 @@ let getQuestion = async () => {
         nextQuestionBtn.classList.remove('isVisible');
         questionTitle.innerHTML = question[questionNumber].question;
 
+        btnNew.addEventListener('click', function() {
+            waitingData();
+        })
+
         //RANDOMIZE ANSWERS ARRAY
         shuffle(question[questionNumber].incorrect_answers);
 
         //CLEAN ANSWERS BOX
         questionAnswers.innerHTML = '';
         
-        questionCount.textContent = `Question : ${questionNumber+1}`
-        scoreText.textContent = `Score : `
+        questionCount.textContent = `Question : ${questionNumber+1}/${question.length}`;
+        scoreText.textContent = `Score : ${score}`;
 
         for(let i=0; i<4 ; i++) {
             let divBtn = document.createElement('div');
@@ -69,118 +77,35 @@ let getQuestion = async () => {
         let iCorrect = document.createElement('i');
         iCorrect.classList.add('fas', 'fa-check');
 
-        // let btnNext = document.querySelector('.btn-next');
+        let iNotCorrect = document.createElement('i');
+        iNotCorrect.classList.add('fas', 'fa-times');
+
 
         let buttons = document.querySelectorAll('.btn-answers');
         buttons.forEach(function(currentBtn) {
-            currentBtn.addEventListener('click', function(e){
+            currentBtn.addEventListener('click', function (e){
                 answer = e.target.textContent;
                 if(answer === question[questionNumber].correct_answer) {
-                    console.log('Correct!!');
+                    score += 1;
                     currentBtn.classList.add('correct');
                     currentBtn.insertAdjacentElement('afterend', iCorrect);
+
+                } else {
+                    currentBtn.classList.add('incorrect');
+                    currentBtn.insertAdjacentElement('afterend', iNotCorrect);
+                }
+
+                questionNumber ++;
+                if (questionNumber < 10) {
                     nextQuestionBtn.classList.add('isVisible');
+                    nextQuestionBtn.addEventListener('click', function(){
+                        renderQuestion(questionNumber);
+                    })
+                } else {
+                    nextQuestionBtn.classList.remove('isVisible');
+                    endGameH2.textContent = `Your final score is : ${score}`;
                 }
             })
         })
 
-        nextQuestionBtn.addEventListener('click', function(){
-            questionNumber ++;
-            if (questionNumber < 10) {
-            renderQuestion(questionNumber);
-            } else {
-                console.log('Questions finished')
-            }
-        })
-
     }
-
-
-
-
-    // console.log(question)
-
-// let quiz = {
-//     question : '',
-//     answers: [],
-//     correctAnsw: ''
-// };
-
-
-// let getQuestion = async () => {
-//     const response = await fetch(`https://opentdb.com/api.php?amount=10&category=22&type=multiple`);
-//     if (response.status === 200) {
-//         let data = await response.json();
-//         quiz.category = data.results[0].category;
-//         quiz.question = data.results[0].question;
-//         quiz.answers = data.results[0].incorrect_answers;
-//         quiz.answers.push(data.results[0].correct_answer);
-//         quiz.correctAnsw = data.results[0].correct_answer;
-//         console.log(data.results);
-//         return data.results;
-//     } else {
-//         throw new Error('Unable to fetch');
-//     }
-// }
-
-
-// // RANDOMIZE ANSWERS ARRAY
-// function shuffle(a) {
-//     for (let i = a.length - 1; i > 0; i--) {
-//         const j = Math.floor(Math.random() * (i + 1));
-//         [a[i], a[j]] = [a[j], a[i]];
-//     }
-//     return a;
-// }
-
-
-// // RENDER QUIZ TO DOM
-// let renderQuestion = async () => {
-
-//     await getQuestion();
-//     console.log(quiz);
-//     shuffle(quiz.answers);
-
-//     let h3 = document.querySelector('.question-title');
-//     h3.textContent = quiz.question;
-//     document.querySelector('.answers-box').innerHTML = '';
-
-//     let id = 0;
-//     quiz.answers.forEach(answer => {
-        
-//         let btn = document.createElement('button');
-//         btn.classList.add('btn-style')
-//         btn.textContent = answer;
-//         btn.id = id;
-//         document.querySelector('.answers-box').appendChild(btn);
-//         id ++;
-//     });
-
-//     game();
-
-//     document.querySelector('.btn-new').addEventListener('click', function(e){
-//         renderQuestion();
-//     })
-// }
-
-
-// let game = () => {
-//         let p = document.createElement('p');
-//         let btn = document.querySelector('.btn-style')
-//         btn.addEventListener('click', function(e) {
-//         let guess = btn.textContent;
-        
-//         if(guess === quiz.correctAnsw) {
-//             console.log('Correct!!')
-//             btn.classList.add('correct')
-//             p.textContent = 'Correct!!'
-//             document.querySelector('body').appendChild(p);
-//         } else {
-//             console.log('Try Again')
-//             btn.classList.add('incorrect')
-//         }
-//     })
-// }
-
-// renderQuestion();
-
